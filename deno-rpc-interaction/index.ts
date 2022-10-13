1a02d6a4a6e48a63007135f2dc1515faebebd8ee
chemonoworld <jinwoo0451@naver.com>
2022-10-14 02:06:19 +0900

    feat: add mnemonic generator

--- deno-rpc-interaction/index.ts
+++ deno-rpc-interaction/index.ts
@@ -1,63 +1,65 @@
 import { Keyring } from "https://deno.land/x/polkadot@0.0.9/keyring/mod.ts";
 import {
-    ApiPromise,
-    WsProvider,
+  ApiPromise,
+  WsProvider,
 } from "https://deno.land/x/polkadot@0.0.9/api/mod.ts";
 import { KeyringPair } from "https://deno.land/x/polkadot@0.0.9/keyring/types.ts";
-import { RPC_ENDPOINT, SS58_FORMAT } from "./BTA1stProject-06/enum.ts";
+import { RPC_ENDPOINT, SS58_FORMAT } from "./enum.ts";
 import { SubmittableExtrinsic } from "https://deno.land/x/polkadot@0.0.9/api/submittable/types.ts";
 import { CodeSubmittableResult } from "https://deno.land/x/polkadot@0.0.9/api-contract/base/Code.ts";
 import { BlueprintSubmittableResult } from "https://deno.land/x/polkadot@0.0.9/api-contract/base/Blueprint.ts";
 import { ApiTypes } from "https://deno.land/x/polkadot@0.0.9/api-base/types/index.ts";
-import type { } from "https://deno.land/x/polkadot@0.0.9/api-augment/mod.ts";
+import type {} from "https://deno.land/x/polkadot@0.0.9/api-augment/mod.ts";
 import type { AnyJson } from "https://deno.land/x/polkadot@0.0.9/types-codec/types/index.ts";
+import { mnemonicGenerate } from "https://deno.land/x/polkadot@0.2.10/util-crypto/mod.ts";
 
 export const getPairFromSeed = (mnemonic: string): KeyringPair => {
-    const seed: string = mnemonic;
-    const keyring: Keyring = new Keyring({ type: "sr25519" });
-    const pair: KeyringPair = keyring.addFromUri(seed);
-    return pair;
+  const seed: string = mnemonic;
+  const keyring: Keyring = new Keyring({ type: "sr25519" });
+  const pair: KeyringPair = keyring.addFromUri(seed);
+  return pair;
 };
 
 export const getPairFromSeedWithSS58 = (
-    mnemonic: string,
-    format: SS58_FORMAT,
+  mnemonic: string,
+  format: SS58_FORMAT,
 ): KeyringPair => {
-    const seed: string = mnemonic;
-    const keyring: Keyring = new Keyring({ type: "sr25519" });
-    keyring.setSS58Format(format);
-    const pair: KeyringPair = keyring.addFromUri(seed);
-    return pair;
+  const seed: string = mnemonic;
+  const keyring: Keyring = new Keyring({ type: "sr25519" });
+  keyring.setSS58Format(format);
+  const pair: KeyringPair = keyring.addFromUri(seed);
+  return pair;
 };
 
 export const getFreeBalance = async (
-    fullNodeUri: string,
-    address: string,
+  fullNodeUri: string,
+  address: string,
 ): Promise<bigint> => {
-    const provider: WsProvider = new WsProvider(fullNodeUri);
-    const api: ApiPromise = await ApiPromise.create({ provider });
-    const { data: balance } = await api.query.system.account(address);
-    const freeBalance = BigInt(balance?.free.toHuman().replace(/\,/g, ""));
-    // 1 ROC = 1,000,000,000,000,000, 1 SBY = 1,000,000,000,000,000,000
-    return freeBalance;
+  const provider: WsProvider = new WsProvider(fullNodeUri);
+  const api: ApiPromise = await ApiPromise.create({ provider });
+  const { data: balance } = await api.query.system.account(address);
+  const freeBalance = BigInt(balance?.free.toHuman().replace(/\,/g, ""));
+  // 1 ROC = 1,000,000,000,000,000, 1 ASTR = 1,000,000,000,000,000,000
+  return freeBalance;
 };
 
 export const transferNativeToken = async (
-    fullNodeUri: string,
-    mnemonic: string,
-    to: string,
-    amountInUnits: bigint,
+  fullNodeUri: string,
+  mnemonic: string,
+  to: string,
+  amountInUnits: bigint,
 ): Promise<string> => {
-    const provider = new WsProvider(fullNodeUri);
-    const api: ApiPromise = await ApiPromise.create({ provider });
-    const pair: KeyringPair = getPairFromSeed(mnemonic);
-    const transfer = api.tx.balances.transfer(to, amountInUnits);
-    const hash = await transfer.signAndSend(pair);
-    return hash.toHex();
+  const provider = new WsProvider(fullNodeUri);
+  const api: ApiPromise = await ApiPromise.create({ provider });
+  const pair: KeyringPair = getPairFromSeed(mnemonic);
+  const transfer = api.tx.balances.transfer(to, amountInUnits);
+  const hash = await transfer.signAndSend(pair);
+  return hash.toHex();
 };
 
 const test = () => {
-    console.log('init');
-}
+  console.log("init");
+  console.log(mnemonicGenerate());
+};
 
 test();
