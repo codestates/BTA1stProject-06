735f9f7528409820ddf87ec7dd8bed176b95e08c
chemonoworld <jinwoo0451@naver.com>
2022-10-14 01:59:03 +0900

    feat: implement rpc-interaction methods

---
+++ deno-rpc-interaction/enum.ts
@@ -0,0 +1,18 @@
+export const enum RPC_ENDPOINT {
+    ROCOCO = "wss://rococo-contracts-rpc.polkadot.io:443",
+    SHIBUYA = "wss://shibuya-rpc.dwellir.com:443",
+    ASTAR = "wss://astar-rpc.dwellir.com:443",
+}
+
+export const enum SS58_FORMAT {
+    POLKADOT = 0,
+    KUSAMA = 2,
+    ASTAR = 5,
+    DEFAULT = 42,
+}
+
+export const enum DECIMAL_PLACE_FROM_PLANCK {
+    POLKADOT = 10,
+    ROCOCO = 12,
+    SHIBUYA = 18,
+}
