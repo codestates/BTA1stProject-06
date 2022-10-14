1c93257c1908c4284898c55971969f53e52ddc18
chemonoworld <jinwoo0451@naver.com>
2022-10-14 19:52:03 +0900

    fix: add rpc endpoints

--- deno-rpc-interaction/enum.ts
+++ deno-rpc-interaction/enum.ts
@@ -1,13 +1,19 @@
 export const enum RPC_ENDPOINT {
-  ROCOCO = "wss://rococo-contracts-rpc.polkadot.io:443",
-  SHIBUYA = "wss://shibuya-rpc.dwellir.com:443",
+  POLKADOT = "wss://rpc.polkadot.io:443",
+  KUSAMA = "wss://kusama-rpc.polkadot.io:443",
   ASTAR = "wss://astar-rpc.dwellir.com:443",
+  SHIDEN = "wss://shiden-rpc.dwellir.com:443",
+  ACALA = "wss://acala-rpc.dwellir.com",
+  PARALLEL = "wss://parallel.api.onfinality.io/public-ws:443",
+  INTERLAY = "wss://interlay.api.onfinality.io/public-ws:443",
+  ROCOCO = "wss://rococo-rpc.polkadot.io:443",
+  SHIBUYA = "wss://shibuya-rpc.dwellir.com:443",
 }
 
 export const enum SS58_FORMAT {
   POLKADOT = 0,
   KUSAMA = 2,
-  ASTAR = 5,
+  ASTAR_FAMILY = 5,
   DEFAULT = 42,
 }
 
