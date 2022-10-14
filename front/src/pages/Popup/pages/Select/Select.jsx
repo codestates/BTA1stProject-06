e81baabe8de8267af511880e190ffbfb6921460d
chaesueryong <chaesueryong@gmail.com>
2022-10-14 20:00:25 +0900

    chrome extension default setting

---
+++ front/src/pages/Popup/pages/Select/Select.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Select.css';
+
+const Select = () => {
+    return (
+        <div className="Select">
+            <div>
+                <div>아니요. 이미 비밀 복구 구문이 있습니다.</div>
+            </div>
+        </div>
+    );
+
+};
+
+export default Select;
---
+++ front/src/pages/Popup/pages/Start/Start.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Start.css';
+
+const Start = () => {
+    return (
+        <div className="Start">
+            <div className="title"><span className="title-main">A-MAN</span> 방문을 환영합니다</div>
+
+            <div className="sub-title">아스타 및 분산형 웹에 연결합니다.</div>
+            <div className="sub-title sub-title1">반갑습니다.</div>
+
+            <button className="start-btn">시작하기</button>
+        </div>
+    );
+
+};
+
+export default Start;
