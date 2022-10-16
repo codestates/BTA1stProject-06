ae792bc39730865210f8307366050f6c52ff9dc7
chaesueryong <chaesueryong@gmail.com>
2022-10-16 20:20:27 +0900

    add balance

--- front/src/pages/Popup/pages/MyWallet/MyWallet.jsx
+++ front/src/pages/Popup/pages/MyWallet/MyWallet.jsx
@@ -1,28 +1,41 @@
 import React, {useEffect} from 'react';
 import './MyWallet.css';
-import {pageState, selectedPairState} from "../../recoil";
+import {chainState, pageState, selectedPairState} from "../../recoil";
 import {useRecoilValue, useSetRecoilState} from "recoil";
 import Header from "../../containers/Header/Header";
+import { getFreeBalance, RpcEndpoint } from '../../modules/usePolkadotAPI';
+import { useState } from 'react';
 
 const MyWallet = () => {
     const setPage = useSetRecoilState(pageState);
+    const chain = useRecoilValue(chainState);
     const selectedPair = useRecoilValue(selectedPairState);
+
+    const [balance, setBalance] = useState('');
 
     useEffect( () => {
         console.log(selectedPair)
+
+        getBalance();
     }, [])
+
+    const getBalance = async () => {
+        const endpoint = RpcEndpoint[chain];
+        const balance = await getFreeBalance(endpoint, selectedPair.address);
+        setBalance(balance.toString());
+    }
 
     return (
         <div className="MyWallet">
             <Header align="left"></Header>
 
             <div className="wallet-info-box1">
-                <div>Account</div>
+                <div></div>
                 <div>{selectedPair.address}</div>
             </div>
 
             <div className="wallet-info-box2">
-
+                {balance === '' ? 'loading...' : balance}
             </div>
 
             <div className="btn-box">
